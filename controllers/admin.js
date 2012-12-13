71b69a045a9b84abab6ab4e3ed7a5513ad8ed3b7
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2012-12-12 19:59:22 -0600

    agregar dependencia

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,41 +8,4 @@ exports.index = function (req, res, next) {
   } else {
     res.redirect('/');
   }
-  
-/*    invoke(function (data, callback) {
-      Message.count(callback);
-    })
-    .and(function (data, callback) {
-      Message.countDesactivados(callback);
-    })
-    .and(function (data, callback) {
-      Message.countPublished(callback);
-    })
-    // datos de usuarios
-    .and(function (data, callback) {
-      User.count(callback);
-    })
-    .and(function (data, callback) {
-      User.find(callback);
-    }).and(function (data, callback) {
-      User.countDesactivados(callback);
-    }).and(function (data, callback) {
-      User.countTwitter(callback);
-    })
-    .and(function (data, callback) {
-      User.countFacebook(callback);
-    })
-    .end(null, function (data) {
-      res.render('admin', { mensajes: {
-        total: data[0],
-        desactivados: data[1],
-        publicados: data[2]
-      }, usuarios: {
-        total: data[3],
-        all: data[4],
-        desactivados: data[5],
-        twitter: data[6],
-        facebook: data[7]
-      }});
-    });*/
 };
