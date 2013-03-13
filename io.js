97dd6b0941e8fa1f80a83cce2879c423e20aa326
Juan Pablo Rojas <jpablorp@warebeone.com>
2013-03-13 17:15:38 -0500

    Comentarios

--- io.js
+++ io.js
@@ -6,22 +6,23 @@ var cookie = require('cookie'),
 
 module.exports = function (config, server, sessionStore) {
   /*
-   * Socket.io configuration
+   * Socket.io configuracion
    */
   var io = require('socket.io').listen(server),
     pub = redis.createClient(),
     sub = redis.createClient(),
     client = redis.createClient();
-
   io.configure(function () {
     io.set('static', new (require('socket.io').Static)(io));
-
+    
+/* Establesco el Redis en el socket con la informacon del cliente */
     io.set('store', new RedisStore({
       redisPub: pub,
       redisSub: sub,
       redisClient: client
     }));
-
+    
+    /* Configuracion de autorizacion de socket en cuanto al cliente con la informacion tomada de sessionStore */
     io.set('authorization', function (data, accept) {
       if(data.headers.cookie) {
         data.cookie = cookie.parse(data.headers.cookie);
@@ -45,13 +46,14 @@ module.exports = function (config, server, sessionStore) {
       }
     });
   });
-
+  
+/* Configuracion de socket ( Envios al cliente ) */
   io.configure('production', function () {
     io.set('log level', 0);
 
-    io.enable('browser client minification'); // send minified client
-    io.enable('browser client etag');         // apply etag caching logic based on version number
-    io.enable('browser client gzip');       // gzip the file
+    io.enable('browser client minification'); // Enviar al cliente minified
+    io.enable('browser client etag');         // aplica lógica de almacenamiento en caché basado en el número de versión
+    io.enable('browser client gzip');       // gzip Archivo
 
     io.set('transports', ['websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling']);
   });
